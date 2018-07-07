16734a2279b49eb5be09b1c5587e9e3f97f2cc55
theteladras <sanel.hadzini@yahoo.com>
2018-07-07 17:24:01 +0200

    work on client side

--- services/passport.js
+++ services/passport.js
@@ -20,18 +20,14 @@ passport.use(new GoogleStrategy({
     clientSecret: keys.googleClientSecret,
     callbackURL: '/auth/google/callback',
     proxy: true
-  }, (accessToken, refreshToken, profile, done) => {
-    User.findOne({ googleId: profile.id })
-      .then((existingUser) => {
-        if (existingUser) {
-          console.log('have the user in records');
-          done(null, existingUser);
-        }
-        else {
-          new User({ googleId: profile.id })
-            .save()
-            .then(user => done(null, user));
-        }
-      })
+  }, 
+  async (accessToken, refreshToken, profile, done) => {
+    const existingUser = await User.findOne({ googleId: profile.id })
+    if (existingUser) {
+      console.log('have the user in records');
+      return done(null, existingUser);
+    }
+    const user = await new User({ googleId: profile.id }).save();
+    done(null, user);
   })
 );
