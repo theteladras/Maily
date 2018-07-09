2ec1d07f626cc82f296ee7a2e11a21eb38e5602b
theteladras <sanel.hadzini@yahoo.com>
2018-07-09 19:40:41 +0200

    client side auth flow done

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
+import Header from './Header';
+import Landing from './Landing';
 
-const App = () => {
-  return (
-    <div>
-      Hi there!
-    </div>
-  );
-};
 
-export default App;
+const Dashboard = () => <h2>Dashboard</h2>
+const SurveyNew = () => <h2>SurveyNew</h2>
+
+class App extends Component {
+  componentDidMount() {
+    this.props.fetchUser();
+  }
+
+  render() {
+    return (
+      <div>
+        <BrowserRouter>
+          <div  className="container">
+            <Header />
+            <Route exact path="/" component={Landing} />
+            <Route exact path="/surveys" component={Dashboard} />
+            <Route path="/surveys/new" component={SurveyNew} />
+          </div>
+        </BrowserRouter>
+      </div>
+    );
+  }
+}
+
+export default connect(null, actions)(App);
