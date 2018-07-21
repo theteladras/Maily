1685d0bd5c4802c36de675d1b3669537521c05e8
theteladras <sanel.hadzini@yahoo.com>
2018-07-22 00:04:38 +0200

    survey sending done

---
+++ services/emailTemplates/surveyTemplate.js
@@ -0,0 +1,19 @@
+const keys = require('../../config/keys');
+
+module.exports = (survey) => {
+  return `
+    <html>
+      <body>
+        <div style="text-align: center; border-style: solid; border-width: 5px; padding: 20px;" class="container">
+          <h3>Give us Your feedback</h3>
+          <p>Please answer the following question:</p>
+          <p>${survey.body}</p>
+          <div>
+            <a href="${keys.redirectDomain}/api/surveys/thanks" style="margin: 0px 20px">Yes</a>
+            <a href="${keys.redirectDomain}/api/surveys/thanks" style="margin: 0px 20px">No</a>
+          </div>
+        </div>
+      </body>
+    </html>
+  `;
+};
